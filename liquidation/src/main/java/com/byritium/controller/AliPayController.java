18e5c9d6968bdb293fd8be0d8144e99ff93ab4c8
byritium <8235303+byritium@user.noreply.gitee.com>
2021-09-13 16:38:03 +0800

    ~

--- liquidation/src/main/java/com/byritium/controller/AliPayController.java
+++ liquidation/src/main/java/com/byritium/controller/AliPayController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LiquidationController {
+public class AliPayController {
 
-    @RequestMapping()
+    @RequestMapping("")
     public void action(String businessOrderId) {
 
     }
---
+++ liquidation/src/main/java/com/byritium/controller/WechatPayController.java
@@ -0,0 +1,12 @@
+package com.byritium.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WechatPayController {
+    @RequestMapping("")
+    public void action(String businessOrderId) {
+
+    }
+}
