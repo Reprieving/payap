f29c01b3fd659fda4302fe6c97e81d9b07312e00
Reprieving <shopping4lee@gmail.com>
2022-02-10 17:45:37 +0800

    ~

--- transaction/src/main/java/com/byritium/controller/TransactionController.java
+++ transaction/src/main/java/com/byritium/controller/TransactionController.java
@@ -4,6 +4,7 @@ import com.byritium.dto.*;
 import com.byritium.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,17 +17,17 @@ public class TransactionController {
     private TransactionService transactionService;
 
     @RequestMapping("create")
-    public TransactionResult guarantee(@RequestBody TransactionRequest transaction) {
+    public TransactionResult guarantee(@RequestBody TransactionRequest request) {
         return null;
     }
 
     @RequestMapping("query")
-    public TransactionResult query() {
+    public TransactionResult query(@RequestBody TransactionRequest request) {
         return null;
     }
 
-    @RequestMapping("notice")
-    public Object notice() {
+    @RequestMapping("notice/{channel}")
+    public Object notice(@PathVariable("channel") String channel) {
         return null;
     }
 }
