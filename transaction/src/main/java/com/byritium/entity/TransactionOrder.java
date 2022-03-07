b762b9ad5bb71139266105b733666129b002d2c7
Reprieving <shopping4lee@gmail.com>
2022-03-07 21:56:37 +0800

    ~

--- transaction/src/main/java/com/byritium/entity/TransactionOrder.java
+++ transaction/src/main/java/com/byritium/entity/TransactionOrder.java
@@ -1,19 +1,25 @@
 package com.byritium.entity;
 
+import com.byritium.constance.PaymentChannel;
+import com.byritium.constance.PaymentState;
+import com.byritium.constance.TransactionState;
 import com.byritium.constance.TransactionType;
 import lombok.Data;
+
 import java.math.BigDecimal;
-import java.sql.Timestamp;
 
 @Data
-public class TransactionBusinessPayModel {
+public class TransactionOrder extends CommonEntity {
     private String id;
     private String clientId;
+    private String accountCoreId;
     private String businessOrderId;
     private TransactionType transactionType;
     private String payerId;
     private String payeeId;
     private BigDecimal orderAmount;
     private BigDecimal payAmount;
-    private Timestamp createTime;
+    private TransactionState transactionState;
+    private PaymentChannel paymentChannel;
+    private PaymentState paymentState;
 }
