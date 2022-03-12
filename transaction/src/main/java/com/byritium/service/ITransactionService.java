ad12178144a501b752434bae6af8eaa4ca422be9
Reprieving <shopping4lee@gmail.com>
2022-03-12 23:55:53 +0800

    ~

--- transaction/src/main/java/com/byritium/service/ITransactionService.java
+++ transaction/src/main/java/com/byritium/service/ITransactionService.java
@@ -3,9 +3,11 @@ package com.byritium.service;
 import com.byritium.constance.TransactionType;
 import com.byritium.dto.TransactionParam;
 import com.byritium.dto.TransactionResult;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public interface ITransactionService {
     TransactionType type();
 
     TransactionResult call(String clientId, TransactionParam param);
+
 }
