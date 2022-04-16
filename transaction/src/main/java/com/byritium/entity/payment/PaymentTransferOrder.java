adf3e0ce89cd09876fb03f436e4867f68f6dc8a9
Reprieving <shopping4lee@gmail.com>
2022-04-16 23:48:24 +0800

    ~

--- transaction/src/main/java/com/byritium/entity/payment/PaymentTransferOrder.java
+++ transaction/src/main/java/com/byritium/entity/payment/PaymentTransferOrder.java
@@ -1,7 +1,17 @@
 package com.byritium.entity.payment;
 
+import com.byritium.constance.PaymentChannel;
 import lombok.Data;
+
+import java.math.BigDecimal;
 
 @Data
 public class PaymentTransferOrder {
+    private String id;
+    private String userId;
+    private String subject;
+    private String trxOrderId;
+    private String bizOrderId;
+    private BigDecimal orderAmount;
+    private PaymentChannel paymentChannel;
 }
