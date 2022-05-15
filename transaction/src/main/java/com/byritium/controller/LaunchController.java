2209ee64e8c513817f63ca5b94c8e8d72fa1b1e9
Reprieving <shopping4lee@gmail.com>
2022-05-15 22:50:13 +0800

    ~

--- transaction/src/main/java/com/byritium/controller/LaunchController.java
+++ transaction/src/main/java/com/byritium/controller/LaunchController.java
@@ -1,9 +1,62 @@
 package com.byritium.controller;
 
+import com.byritium.dto.TransactionParam;
+import com.byritium.dto.TransactionResult;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("launch")
 public class LaunchController {
+    @RequestMapping("guarantee")
+    public TransactionResult guarantee(@RequestHeader String clientId, @RequestBody TransactionParam request) {
+        return null;
+    }
+
+    @RequestMapping("instant")
+    public TransactionResult instant(@RequestHeader String clientId, @RequestBody TransactionParam request) {
+        return null;
+    }
+
+    @RequestMapping("recharge")
+    public TransactionResult recharge(@RequestHeader String clientId, @RequestBody TransactionParam request) {
+        return null;
+    }
+
+    @RequestMapping("withdraw")
+    public TransactionResult withdraw(@RequestHeader String clientId, @RequestBody TransactionParam request) {
+        return null;
+    }
+
+    @RequestMapping("transfer")
+    public TransactionResult transfer(@RequestHeader String clientId, @RequestBody TransactionParam request) {
+        return null;
+    }
+
+    @RequestMapping("refund/guarantee")
+    public TransactionResult refundGurantee(@RequestHeader String clientId, @RequestBody TransactionParam request) {
+        return null;
+    }
+
+    @RequestMapping("refund/instant")
+    public TransactionResult refundInstant(@RequestHeader String clientId, @RequestBody TransactionParam request) {
+        return null;
+    }
+
+    @RequestMapping("refund/withdraw")
+    public TransactionResult refundWithdraw(@RequestHeader String clientId, @RequestBody TransactionParam request) {
+        return null;
+    }
+
+    @RequestMapping("refund/recharge")
+    public TransactionResult refundRecharge(@RequestHeader String clientId, @RequestBody TransactionParam request) {
+        return null;
+    }
+
+    @RequestMapping("refund/exception")
+    public TransactionResult refundException(@RequestHeader String clientId, @RequestBody TransactionParam request) {
+        return null;
+    }
 }
