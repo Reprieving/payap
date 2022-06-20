833ae35a77c0c61bf0ed06b01a18be6e2d53024e
Reprieving <shopping4lee@gmail.com>
2022-06-20 23:59:32 +0800

    ~

--- transaction/src/main/java/com/byritium/controller/RefundController.java
+++ transaction/src/main/java/com/byritium/controller/RefundController.java
@@ -10,23 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("refund")
 public class RefundController {
-    @RequestMapping("guarantee")
-    public TransactionResult refundGuarantee(@RequestHeader String clientId, @RequestBody GuaranteeTxReq request) {
-        return null;
-    }
-
-    @RequestMapping("instant")
-    public TransactionResult refundInstant(@RequestHeader String clientId, @RequestBody RefundInstantTxReq request) {
-        return null;
-    }
-
-    @RequestMapping("withdraw")
-    public TransactionResult refundWithdraw(@RequestHeader String clientId, @RequestBody RefundWithdrawTxReq request) {
-        return null;
-    }
-
-    @RequestMapping("recharge")
-    public TransactionResult refundRecharge(@RequestHeader String clientId, @RequestBody RefundRechargeTxReq request) {
+    @RequestMapping("common")
+    public TransactionResult refundCommon(@RequestHeader String clientId, @RequestBody GuaranteeTxReq request) {
         return null;
     }
 
