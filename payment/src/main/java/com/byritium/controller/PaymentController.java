4a92ab032eda3982ba6cbb665aab6ca6ba0cc91d
Reprieving <shopping4lee@gmail.com>
2022-07-07 23:55:19 +0800

    ~

--- payment/src/main/java/com/byritium/controller/PaymentController.java
+++ payment/src/main/java/com/byritium/controller/PaymentController.java
@@ -7,56 +7,60 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class PaymentController {
 
-
-
     @RequestMapping("call")
-    public void call(@RequestBody List<TransactionPayOrder> orders) {
+    public void call(@RequestBody Map<String,Object> param) {
 
     }
 
-    @RequestMapping("pay")
-    public void pay(@RequestBody List<TransactionPayOrder> orders) {
-
-    }
-
-    @RequestMapping("withdraw")
-    public void withdraw(@RequestBody List<TransactionPayOrder> orders) {
-
-    }
-
-    @RequestMapping("transfer")
-    public void transfer() {
-
-    }
-
-    @RequestMapping("refund")
-    public void refund() {
-
-    }
-
-    @RequestMapping("free")
-    public void free() {
-
-    }
-
-    @RequestMapping("unfree")
-    public void unfree() {
-
-    }
-
-    //入款
-    @RequestMapping("income")
-    public void income() {
-
-    }
-
-    //冲账
-    @RequestMapping("reverse")
-    public void reverse() {
-
-    }
+//    @RequestMapping("call")
+//    public void call(@RequestBody List<TransactionPayOrder> orders) {
+//
+//    }
+//
+//    @RequestMapping("pay")
+//    public void pay(@RequestBody List<TransactionPayOrder> orders) {
+//
+//    }
+//
+//    @RequestMapping("withdraw")
+//    public void withdraw(@RequestBody List<TransactionPayOrder> orders) {
+//
+//    }
+//
+//    @RequestMapping("transfer")
+//    public void transfer() {
+//
+//    }
+//
+//    @RequestMapping("refund")
+//    public void refund() {
+//
+//    }
+//
+//    @RequestMapping("free")
+//    public void free() {
+//
+//    }
+//
+//    @RequestMapping("unfree")
+//    public void unfree() {
+//
+//    }
+//
+//    //入款
+//    @RequestMapping("income")
+//    public void income() {
+//
+//    }
+//
+//    //冲账
+//    @RequestMapping("reverse")
+//    public void reverse() {
+//
+//    }
 }
