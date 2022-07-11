0ddb62b6f8e3d8aecfd7ebc1df75edd06e306be3
Reprieving <shopping4lee@gmail.com>
2022-07-11 23:49:23 +0800

    ~

--- payment/src/main/java/com/byritium/controller/PaymentController.java
+++ payment/src/main/java/com/byritium/controller/PaymentController.java
@@ -5,8 +5,6 @@ import com.byritium.dto.order.TransactionPayOrder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 import java.util.Map;
 
 @RestController
---
+++ payment/src/main/java/com/byritium/dto/PaymentParam.java
@@ -0,0 +1,9 @@
+package com.byritium.dto;
+
+import lombok.Data;
+
+@Data
+public class PaymentParam {
+    private String bizOrderId;
+    private String txOrderId;
+}
