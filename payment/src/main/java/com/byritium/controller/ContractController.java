6026b478ea337607ce0995af63c304a7c5c2b584
Reprieving <shopping4lee@gmail.com>
2022-07-15 22:52:42 +0800

    ~

--- payment/src/main/java/com/byritium/controller/ContractController.java
+++ payment/src/main/java/com/byritium/controller/ContractController.java
@@ -6,4 +6,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("contract")
 public class ContractController {
+
+    @RequestMapping("sign")
+    public void sign(){
+
+    }
+
+
+    @RequestMapping("rescind")
+    public void rescind(){
+
+    }
+
+    @RequestMapping("query")
+    public void query(){
+
+    }
 }
