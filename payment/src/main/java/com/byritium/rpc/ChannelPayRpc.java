d784f76d3acd7c5547597f3b1ee9d64f66a5b423
Reprieving <shopping4lee@gmail.com>
2022-07-25 23:06:26 +0800

    ~

---
+++ payment/src/main/java/com/byritium/rpc/ChannelPayRpc.java
@@ -0,0 +1,21 @@
+package com.byritium.rpc;
+
+import com.byritium.dto.PaymentResult;
+import com.byritium.dto.ResponseBody;
+import com.byritium.entity.PayOrder;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient(value = "channel")
+@Component
+public interface ChannelPayRpc {
+
+    @RequestMapping("call")
+    ResponseBody<PaymentResult> call();
+
+    @RequestMapping("query")
+    ResponseBody<PaymentResult> withdraw();
+
+}
