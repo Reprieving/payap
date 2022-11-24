d8cff3cf84bbaecd934b17543c8a01f50e66573a
Reprieving <shopping4lee@gmail.com>
2022-11-24 22:13:25 +0800

    ~

---
+++ payment/src/main/java/com/byritium/compose/directive/Directive.java
@@ -0,0 +1,5 @@
+package com.byritium.compose.directive;
+
+public interface Directive {
+    void execute(Long paymentOrderId);
+}
--- payment/src/main/java/com/byritium/dto/AccountRecordedParam.java
+++ payment/src/main/java/com/byritium/dto/AccountRecordedParam.java
@@ -1,8 +1,10 @@
 package com.byritium.dto;
 
+import com.byritium.BusinessType;
 import lombok.Data;
 
 @Data
 public class AccountRecordedParam {
     private Long paymentOrderId;
+    private BusinessType businessType;
 }
